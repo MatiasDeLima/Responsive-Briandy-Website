5fbee444de3779b8ab3ab1db02c7972897c8c64d
lucas matias <lucasmlima1516@gmail.com>
2023-05-20 20:45:32 -0300

    Briandy

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+/*############### SHOW MENU ###############*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+if(navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
